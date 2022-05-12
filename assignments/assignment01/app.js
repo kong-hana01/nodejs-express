5146e43d559277b0323879ba8791895502ae38a2
최한빈 <gksqlsl11@khu.ac.kr>
2022-05-13 02:23:30 +0900

    homework

--- assignments/assignment01/app.js
+++ assignments/assignment01/app.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 let books = new Array();
 
 app.get('/books/:bookId', (req, res) => {
+	if(!books[req.params.bookId]) {
+		res.send('The bookID was not found.')
+		return;
+	}
 	let bookId = req.params.bookId;
     console.log(books[bookId]);
     res.send(books[bookId]);
@@ -25,19 +29,37 @@ app.get('/books/:bookId', (req, res) => {
 */
 app.post('/books', (req, res) => {
 	// Create book information
+	if(books[req.params.bookId]) {
+		res.send('The book information was created before.')
+		return;
+	}
 	books[req.body.id] = [req.body.id, req.body.name, req.body.price, req.body.author];
 	res.send(books[req.body.id]);
 })
 
-app.put('/books', (req, res) => {
+app.put('/books/:bookId', (req, res) => {
 	// Update book information
-
+	if(!books[req.params.bookId]) {
+		res.status(404).send('The bookID was not found.');
+		return;
+	}
+	if(req.params.bookId != req.body.id) {
+		res.send('BookId from parameter is different with BookId from body.');
+		return;
+	}
+	books[req.params.bookId] = [req.body.id, req.body.name, req.body.price, req.body.author];
+	res.send(books[req.body.id]);
 })
 
 
 app.delete('/books/:bookId', (req, res) => {
 	// Delete book information
-
+	if(!books[req.params.bookId]) {
+		res.status(404).send('The bookID was not found.');
+		return;
+	}
+	books.splice(req.params.bookId, 1);
+	res.send('Delete Success.')
 })
 let server = app.listen(80);
 	console.log(books);
--- assignments/assignment02/app.js
+++ assignments/assignment02/app.js
@@ -21,22 +21,39 @@ app.put('/login', (req, res) => {
 	// admin 여부를 확인하여 체크
 	// req.body.id : ID
 	// req.body.name : 패스워드
-	
-	res.send("Login");
+	if ("userId" in req.session){
+		res.send("이미 로그인중입니다.");
+		return true;
+	}
+
+	if(!(users.some(u => u.userId === req.body.id && u.password === req.body.password))){
+		res.send("잘못된 로그인 정보를 입력하셨습니다.");
+		return false;
+	}
+	req.session.userId = req.body.id;
+	req.session.isAdmin = users.some(u => u.userId === req.body.id && u.isAdmin);
+	req.session.save(function(){
+		res.send("LogIn");
+	});
+	return true;
 });
 
 app.put('/logout', (req, res) => {
 	// Logout
 	// 세션 유효 여부를 체크하고 세션 Delete
-	req.session.userId = null;
-	res.send("LogOut");
-
+	if ("userId" in req.session){
+		req.session.destroy(function(err){});
+		res.send("LogOut");
+		return true;
+	}
+	res.send('로그아웃 상태입니다.');
+	return true;
 });
 
 let auth = (req, res, next) => {
 	// Session Check
 	// 어드민 여부 체크 필요
-	if (req.session.userId != null)
+	if (req.session.userId != null && req.session.isAdmin)
 		next();
 	else
 		res.send("Error");
@@ -44,9 +61,72 @@ let auth = (req, res, next) => {
 };
 app.get('/user/:userId', auth, (req, res) => {
 	// get User Information
-	res.send("OK");
+	if (!(users.some(u => u.userId == req.params.userId))){
+		res.send('사용자 정보가 없습니다.');
+		return false;
+	}
+	let userIndex = users.findIndex((item, idx) => {
+		return item.userId == req.params.userId;
+	})
+	res.send(users[userIndex]);
+	return true;
 });
 
 // 사용자 추가 시에 admin 여부도 추가해야 함
+app.put('/users', auth, (req, res) => {
+	if (!(users.some(u => u.userId == req.body.id))){
+		res.send('사용자 정보가 없습니다.');
+		return false;
+	}
+	if (req.session.userId == req.body.id && !req.body.isAdmin){
+		res.send("잘못된 접근입니다.");
+		return false;
+	}
+
+	let userIndex = users.findIndex((item, idx) => {
+		return item.userId == req.body.id;
+	})
+	users[userIndex] = {	
+		"userId" : req.body.id,
+		"name" : req.body.name,
+		"password" : req.body.password,
+		"isAdmin" : req.body.isAdmin
+	}
+	res.send("사용자 정보 업데이트를 성공했습니다.");
+	return true;
+});
+
+app.post('/users', auth, (req, res) => {
+	if (users.some(u => u.userId == req.body.id)){
+		res.send('이미 존재하는 사용자 정보입니다.');
+		return false;
+	}
+	users[users.length] = {	
+		"userId" : req.body.id,
+		"name" : req.body.name,
+		"password" : req.body.password,
+		"isAdmin" : req.body.isAdmin
+	}
+	res.send("사용자 정보를 추가했습니다.");
+	return true;
+});
+
+app.delete('/users/:userId', auth, (req, res) => {
+	if (!(users.some(u => u.userId == req.params.userId))){
+		res.send('사용자 정보가 없습니다.');
+		return false;
+	}
+	if (req.session.userId == req.params.userId){
+		res.send("잘못된 접근입니다.");
+		return false;
+	}
+	let userIndex = users.findIndex((item, idx) => {
+		return item.userId == req.params.userId;
+	})
+	let spliceResult = users.splice(userIndex, 1);
+	res.send("사용자 정보를 삭제했습니다.");
+	return true;
+});
+
 
 let server = app.listen(80);
